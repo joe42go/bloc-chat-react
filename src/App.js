c715a401020445c1a261a0b9fd99908ac248f217
Joe Chung <jchung79@hotmail.com>
2019-05-23 18:47:04 -0700

    error on line 24 of RoomList.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+<script src="https://www.gstatic.com/firebasejs/6.0.3/firebase-app.js"></script>
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAifkcLWNWHk81t8ZddkfCmevkCZN9NppE",
+    authDomain: "create-react-app-79547.firebaseapp.com",
+    databaseURL: "https://create-react-app-79547.firebaseio.com",
+    projectId: "create-react-app-79547",
+    storageBucket: "create-react-app-79547.appspot.com",
+    messagingSenderId: "969062752817",
+    appId: "1:969062752817:web:78dc005cdd902c6d"
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RoomList /> 
   );
 }
 
---
+++ src/components/RoomList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rooms: []
+        };
+
+        this.roomsRef = this.props.firebase.database().ref('rooms');
+    }
+
+    componentDidMount() {
+        this.roomsRef.on('child-added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({ rooms: this.state.rooms.concat (room) })
+        });
+    }
+
+    render () {
+        return (
+            {this.state.rooms.map( (room, index) =>
+                <div>{room}</div>} 
+            )
+        );
+    }
+}
+
+
+export default RoomList;
